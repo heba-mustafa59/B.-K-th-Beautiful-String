fa1ca84ed7a4068820dbc646f119db57c113fa1f
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-21 12:32:15 +0300

    Add files via upload
    B. K-th Beautiful String codeforces solution

---
+++ B. K-th Beautiful String.cpp
@@ -0,0 +1,28 @@
+﻿/* yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t, n1, n2, n3;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        n3 = 1;
+        cin >> n1 >> n2;
+        string s(n1, 'a');
+
+        for (; n2 > n3; n3++) {
+            n2 -= n3;
+        }
+
+        s[n1 - n3 - 1] = 'b';
+        s[n1 - n2] = 'b';
+
+        cout << s << "\n";
+    }
+
+    return 0;
+}
